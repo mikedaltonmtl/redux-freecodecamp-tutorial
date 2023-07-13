9aa1a51979dd35a39e1a09549d85c336e58e1e2b
mike-dalton <michael.j.dalton@gmail.com>
2023-07-13 13:35:16 -0400

    completed tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + action.payload;
+    case 'DECREMENT':
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log('current state', store.getState());
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+  payload: 1
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+  payload: 5
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  payload: 2
+});
